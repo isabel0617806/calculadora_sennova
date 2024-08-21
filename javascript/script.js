935c70992bbc844be1e09cb8842204371e933fc3
isabel0617806 <mtoro8130@gmail.com>
2024-08-21 15:04:20 -0500

    Se realizaron modificaciones en Java

--- javascript/script.js
+++ javascript/script.js
@@ -1,118 +1,46 @@
-html, body {
-  height: 100%;
-  margin: 0;
-}
 
-body {
-  display: flex;
-  flex-direction: column;
-}
+const calculadora = document.getElementById('calculadora')
+const resultado = document.getElementById('resultado')
 
-header {
-  height: 100px;
-  width: auto;
-  background-color: #39A136;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-    sans-serif;
-  padding: 10px; /* Ajusta el padding si es necesario */
-  margin: 20px;
-  position: relative; /* Necesario para que los elementos hijos se posicionen en relación con el header */
-}
+calculadora.addEventListener('click', añadirNumeros)
 
-header img {
-  position: absolute;
-  top: 10px; /* Ajusta la distancia desde el borde superior */
-  left: 10px; /* Ajusta la distancia desde el borde izquierdo */
-}
+let operaciones = []
 
-main {
-  flex: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-#calculadora table {
-  height: 300px;
-  margin: 10px;
-  margin: auto;
-}
-
-#calculadora button,
-#calculadora input {
-  width: 100%;
-  height: 100%;
-  font-size: 1em;
-  padding: 10px;
-}
-
-#calculadora input {
-  padding: 5%;
-  margin: 0.95px;
-  color: white;
-  border: none;
-  background-color: rgba(0, 0, 0, .4);
-  border-radius: 10px;
-  font-size: 25px; 
-  padding: 20px;
-  cursor: pointer; 
-  width: 100%;
-  height: 65px;
-}
-
-#calculadora button {
-  background-color: rgba(0, 0, 0, .5);
-  border-radius: 10px;
-  border: 1px;
-  color: white;
-  font-weight: bold;
-  cursor: pointer;
-}
-
-#calculadora button:hover {
-  background-color: rgba(0, 0, 0, .5);
-  border-radius: 10px;
-}
-
-#calculadora button.operacion {
-  background-color: rgb(36, 39, 231);
-  border-radius: 10px;
-}
-
-#calculadora button.operacion#restar,
-#calculadora button.operacion#clear {
-  background-color: rgb(36, 39, 231);
-  border-radius: 10px;
-}
-
-#calculadora button.operacion#igual {
-  background-color: red;
-  border-radius: 10px;
-}
-
-footer {
-  height: 120px;
-  width: 100%;
-  color: white;
-  background-color: #39A136;
-  font-family: Arial, Helvetica, sans-serif;
-  padding: 1rem 2rem;
-  margin: 0;
-  box-sizing: border-box;
-  text-align: center;
-}
-
-.footer-imageceet {
-  max-width: 300px; 
-  height: auto;
-  float: left;
-  margin: 20px; 
-}
-
-.footer-imagesennova {
-  max-width: 200px; 
-  height: auto; 
-  float: right; 
-  margin-left: 20px; 
+function añadirNumeros(e){
+	if(e.target.getAttribute('type') === 'button'){
+		if(e.target.className != 'operacion'){
+			resultado.value += e.target.innerText
+		}
+		if(e.target.id === 'sumar'){
+			operaciones.push(resultado.value)
+			operaciones.push('+')
+			calculadora.reset()
+		}
+		if(e.target.id === 'igual'){
+			operaciones.push(resultado.value)
+			calculadora.reset()
+			const resultadoOperacion = eval(operaciones.join(''))
+			resultado.value = resultadoOperacion
+			operaciones = []
+		}
+		if(e.target.id === 'restar'){
+			operaciones.push(resultado.value)
+			operaciones.push('-')
+			calculadora.reset()
+		}
+		if(e.target.id === 'multiplicar'){
+			operaciones.push(resultado.value)
+			operaciones.push('*')
+			calculadora.reset()
+		}
+		if(e.target.id === 'dividir'){
+			operaciones.push(resultado.value)
+			operaciones.push('/')
+			calculadora.reset()
+		}
+		if(e.target.id === 'clear'){
+			operaciones = []
+			calculadora.reset()
+		}
+	}
 }
